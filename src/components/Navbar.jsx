ef4c576d23f7552033168d8b5d268d50982942c6
estherpet <estheriyowogoga@gmail.com>
2024-02-26 23:34:07 -0800

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,11 +10,8 @@ const Navbar = () => {
           <h3>E-commerce Product Page</h3>
           <div className='nav-container'>
             <CartIcon />
-            {/*<div className='amount-container'>*/}
-            {/*  <p className='total-amount'>{amount}</p>*/}
             </div>
           </div>
-        {/*</div>*/}
       </nav>
     </>
   );
